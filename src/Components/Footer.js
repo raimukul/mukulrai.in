b48fe3bac3cd796f08f568c3fa4795c5491c49d0
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2021-02-12 10:36:52 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021</li>
+              <li>Design by <a title="Styleshout" href="https://www.mukulrai.in">Mukul Rai</a></li>
            </ul>
 
         </div>
